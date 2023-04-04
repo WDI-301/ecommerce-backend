91dbeb870c20d1119c68f3728bc5d43bf68387d9
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-04-04 13:03:33 -0700

    added cors and dotenv:

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var cors = require('cors')
 var app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+  }
+))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
