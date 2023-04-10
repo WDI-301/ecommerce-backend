4273232252593de2930edf54d54600b8e0f12e1c
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-04-10 07:52:34 -0700

    login route - just returns what it is sent

--- routes/users/controller/userController.js
+++ routes/users/controller/userController.js
@@ -1,15 +1,22 @@
 module.exports = {
     login: async (req,res) => {
         try {
+            let username = req.body.username
+            let password = req.body.password
+            if ( username === 'Violet') {
+                throw new Error('This user exists')
+            }
+            if (password === '' || password === undefined) {
+                throw new Error("password can't be blank" )
+            }
+
             res.status(200).json({
                 message: 'Post request from the Controller',
                 userObj: req.body
               })
         } 
         catch (error) {
-            res.status(500).json({
-                message: error
-              })
+            res.status(409).json({message: error})
         }
     }
 }
